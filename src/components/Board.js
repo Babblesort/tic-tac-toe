0fdf00bc69f760680a5c3f999d407b29fe773e89
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-11-15 19:29:23 -0600

    normalize board props into square indexed arrays

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,22 +2,27 @@ import React from 'react';
 import Square from './Square';
 
 const Board = props => {
+  const indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const indexedProps = indexes.map(i => ({
+    value: props.squares[i],
+    handler: () => props.onClick(i)
+  }));
   return (
     <div>
       <div className="board-row">
-        <Square value={props.squares[0]} onClick={() => props.onClick(0)} />
-        <Square value={props.squares[1]} onClick={() => props.onClick(1)} />
-        <Square value={props.squares[2]} onClick={() => props.onClick(2)} />
+        <Square value={indexedProps[0].value} onClick={indexedProps[0].handler} />
+        <Square value={indexedProps[1].value} onClick={indexedProps[1].handler} />
+        <Square value={indexedProps[2].value} onClick={indexedProps[2].handler} />
       </div>
       <div className="board-row">
-        <Square value={props.squares[3]} onClick={() => props.onClick(3)} />
-        <Square value={props.squares[4]} onClick={() => props.onClick(4)} />
-        <Square value={props.squares[5]} onClick={() => props.onClick(5)} />
+        <Square value={indexedProps[3].value} onClick={indexedProps[3].handler} />
+        <Square value={indexedProps[4].value} onClick={indexedProps[4].handler} />
+        <Square value={indexedProps[5].value} onClick={indexedProps[5].handler} />
       </div>
       <div className="board-row">
-        <Square value={props.squares[6]} onClick={() => props.onClick(6)} />
-        <Square value={props.squares[7]} onClick={() => props.onClick(7)} />
-        <Square value={props.squares[8]} onClick={() => props.onClick(8)} />
+        <Square value={indexedProps[6].value} onClick={indexedProps[6].handler} />
+        <Square value={indexedProps[7].value} onClick={indexedProps[7].handler} />
+        <Square value={indexedProps[8].value} onClick={indexedProps[8].handler} />
       </div>
     </div>
   );
