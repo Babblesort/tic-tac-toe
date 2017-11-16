183d6a655816c4699fa0ffa11cd15d950bc8ede9
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-11-15 18:52:02 -0600

    complete MoveLinks unit test with click handler asserts

--- src/components/MoveLinks.test.js
+++ src/components/MoveLinks.test.js
@@ -23,3 +23,17 @@ it('renders an empty ordered list when history is undefined', () => {
   const liSet = div.querySelectorAll('li');
   expect(liSet.length).toBe(0);
 });
+
+it('hands down click handler to MoveLink with history index as param', () => {
+  const history = ['a', 'b'];
+  const mockHandler = jest.fn();
+  const div = document.createElement('div');
+  ReactDOM.render(<MoveLinks history={history} onClick={mockHandler}/>, div);
+  const moveLinkButtons = div.querySelectorAll('ol button');
+  expect(moveLinkButtons.length).toBe(history.length);
+
+  ReactTestUtils.Simulate.click(moveLinkButtons[1]);
+  expect(mockHandler.mock.calls[0]).toEqual([1]);
+  ReactTestUtils.Simulate.click(moveLinkButtons[0]);
+  expect(mockHandler.mock.calls[1]).toEqual([0]);
+});
